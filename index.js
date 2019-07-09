11c79773083342deab4a14cd829b46605ad4293a
MohsinNajmuddin <mohsin.najmuddin@elastica.co>
2019-07-10 02:00:24 +0500

    Done changes related to messaging and npm request installation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = "EAAJ8YVVvr3YBAPeZCdLnrTZAkc3dROjlDDNXKUM1UaZBXRcFSFfiTBAfVruXWNYHv2EM0eJLZA92sZBcZBHNY2BPEB3Nvcu0E6EWTbnF0NUqtyvoH0O0VGsxP9dPVkMZA7ouRMgkYSZAY76kC8qnrslfS78wnxAZAA7Lp2QnRYjltT1jciIn9O9Ca";
+const request = require('request');
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -25,7 +28,12 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      console.log('Mohsin');
+
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -36,7 +44,6 @@ app.post('/webhook', (req, res) => {
   }
 
 });
-
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
@@ -65,3 +72,52 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+
+  // Sends the response message
+  callSendAPI(sender_psid, response); 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+  
+}

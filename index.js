9cc9242cc9e338ed4185ea59ebf1a2edb673a08a
MohsinNajmuddin <mohsin.najmuddin@elastica.co>
2019-07-13 09:45:21 +0500

    edited the page count

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function handleMessage(sender_psid, received_message) {
       selectedSearchOption = '';
       var searchObj = {
         q: received_message.text,
-        page: 1,
+        page: 5,
         field: 'title'
       };
       gr.searchBooks(searchObj).then(function (response) {
